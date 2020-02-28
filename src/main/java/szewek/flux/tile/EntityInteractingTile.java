f38fbe91177a7ae5c249c023a8bd3c6bf476c80d
Szewek <shevkpl@gmail.com>
2020-02-28 18:02:07 +0100

    Fix AxisAlignedBB not being set after creating a new tile entity

--- src/main/java/szewek/flux/tile/EntityInteractingTile.java
+++ src/main/java/szewek/flux/tile/EntityInteractingTile.java
@@ -4,6 +4,7 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 abstract class EntityInteractingTile extends PoweredTile {
 	protected AxisAlignedBB aabb;
@@ -29,10 +30,18 @@ abstract class EntityInteractingTile extends PoweredTile {
 	}
 
 	@Override
+	public void setWorldAndPos(World w, BlockPos posIn) {
+		super.setWorldAndPos(w, posIn);
+		int x = posIn.getX(), y = posIn.getY(), z = posIn.getZ();
+		aabb = new AxisAlignedBB(x-4, y-4, z-4, x+4, y+4, z+4);
+	}
+
+	@Override
 	public void tick() {
 		if (!world.isRemote) {
-			if (cooldown > 0) --cooldown;
-			else {
+			if (cooldown > 0) {
+				--cooldown;
+			} else {
 				cooldown = 20;
 				interact();
 			}
