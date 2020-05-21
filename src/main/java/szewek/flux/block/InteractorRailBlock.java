430277af53a7d8707fd05528709cb770c7b2d35a
Szewek <shevkpl@gmail.com>
2020-05-21 02:36:46 +0200

    Use Detector Rail methods

--- src/main/java/szewek/flux/block/InteractorRailBlock.java
+++ src/main/java/szewek/flux/block/InteractorRailBlock.java
@@ -137,120 +137,12 @@ public class InteractorRailBlock extends AbstractRailBlock {
 
 	@Override
 	public BlockState rotate(BlockState state, IWorld world, BlockPos pos, Rotation rot) {
-		switch(rot) {
-			case CLOCKWISE_180:
-				switch(state.get(SHAPE)) {
-					case ASCENDING_EAST:
-						return state.with(SHAPE, RailShape.ASCENDING_WEST);
-					case ASCENDING_WEST:
-						return state.with(SHAPE, RailShape.ASCENDING_EAST);
-					case ASCENDING_NORTH:
-						return state.with(SHAPE, RailShape.ASCENDING_SOUTH);
-					case ASCENDING_SOUTH:
-						return state.with(SHAPE, RailShape.ASCENDING_NORTH);
-					case SOUTH_EAST:
-						return state.with(SHAPE, RailShape.NORTH_WEST);
-					case SOUTH_WEST:
-						return state.with(SHAPE, RailShape.NORTH_EAST);
-					case NORTH_WEST:
-						return state.with(SHAPE, RailShape.SOUTH_EAST);
-					case NORTH_EAST:
-						return state.with(SHAPE, RailShape.SOUTH_WEST);
-				}
-			case COUNTERCLOCKWISE_90:
-				switch(state.get(SHAPE)) {
-					case NORTH_SOUTH:
-						return state.with(SHAPE, RailShape.EAST_WEST);
-					case EAST_WEST:
-						return state.with(SHAPE, RailShape.NORTH_SOUTH);
-					case ASCENDING_EAST:
-						return state.with(SHAPE, RailShape.ASCENDING_NORTH);
-					case ASCENDING_WEST:
-						return state.with(SHAPE, RailShape.ASCENDING_SOUTH);
-					case ASCENDING_NORTH:
-						return state.with(SHAPE, RailShape.ASCENDING_WEST);
-					case ASCENDING_SOUTH:
-						return state.with(SHAPE, RailShape.ASCENDING_EAST);
-					case SOUTH_EAST:
-						return state.with(SHAPE, RailShape.NORTH_EAST);
-					case SOUTH_WEST:
-						return state.with(SHAPE, RailShape.SOUTH_EAST);
-					case NORTH_WEST:
-						return state.with(SHAPE, RailShape.SOUTH_WEST);
-					case NORTH_EAST:
-						return state.with(SHAPE, RailShape.NORTH_WEST);
-				}
-			case CLOCKWISE_90:
-				switch(state.get(SHAPE)) {
-					case NORTH_SOUTH:
-						return state.with(SHAPE, RailShape.EAST_WEST);
-					case EAST_WEST:
-						return state.with(SHAPE, RailShape.NORTH_SOUTH);
-					case ASCENDING_EAST:
-						return state.with(SHAPE, RailShape.ASCENDING_SOUTH);
-					case ASCENDING_WEST:
-						return state.with(SHAPE, RailShape.ASCENDING_NORTH);
-					case ASCENDING_NORTH:
-						return state.with(SHAPE, RailShape.ASCENDING_EAST);
-					case ASCENDING_SOUTH:
-						return state.with(SHAPE, RailShape.ASCENDING_WEST);
-					case SOUTH_EAST:
-						return state.with(SHAPE, RailShape.SOUTH_WEST);
-					case SOUTH_WEST:
-						return state.with(SHAPE, RailShape.NORTH_WEST);
-					case NORTH_WEST:
-						return state.with(SHAPE, RailShape.NORTH_EAST);
-					case NORTH_EAST:
-						return state.with(SHAPE, RailShape.SOUTH_EAST);
-				}
-			default:
-				return state;
-		}
+		return Blocks.DETECTOR_RAIL.rotate(state, rot);
 	}
 
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirrorIn) {
-		RailShape railshape = state.get(SHAPE);
-		switch(mirrorIn) {
-			case LEFT_RIGHT:
-				switch(railshape) {
-					case ASCENDING_NORTH:
-						return state.with(SHAPE, RailShape.ASCENDING_SOUTH);
-					case ASCENDING_SOUTH:
-						return state.with(SHAPE, RailShape.ASCENDING_NORTH);
-					case SOUTH_EAST:
-						return state.with(SHAPE, RailShape.NORTH_EAST);
-					case SOUTH_WEST:
-						return state.with(SHAPE, RailShape.NORTH_WEST);
-					case NORTH_WEST:
-						return state.with(SHAPE, RailShape.SOUTH_WEST);
-					case NORTH_EAST:
-						return state.with(SHAPE, RailShape.SOUTH_EAST);
-					default:
-						return super.mirror(state, mirrorIn);
-				}
-			case FRONT_BACK:
-				switch(railshape) {
-					case ASCENDING_EAST:
-						return state.with(SHAPE, RailShape.ASCENDING_WEST);
-					case ASCENDING_WEST:
-						return state.with(SHAPE, RailShape.ASCENDING_EAST);
-					case ASCENDING_NORTH:
-					case ASCENDING_SOUTH:
-					default:
-						break;
-					case SOUTH_EAST:
-						return state.with(SHAPE, RailShape.SOUTH_WEST);
-					case SOUTH_WEST:
-						return state.with(SHAPE, RailShape.SOUTH_EAST);
-					case NORTH_WEST:
-						return state.with(SHAPE, RailShape.NORTH_EAST);
-					case NORTH_EAST:
-						return state.with(SHAPE, RailShape.NORTH_WEST);
-				}
-		}
-
-		return super.mirror(state, mirrorIn);
+		return Blocks.DETECTOR_RAIL.mirror(state, mirrorIn);
 	}
 
 	@Override
