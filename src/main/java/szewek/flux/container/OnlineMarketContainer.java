83a0532c80e8333214a12bd3ac2b3dcaafe65185
Szewek <shevkpl@gmail.com>
2020-06-28 00:15:51 +0200

    Use custom stack transferring method to prevent desyncing; Fixes #32

--- src/main/java/szewek/flux/container/OnlineMarketContainer.java
+++ src/main/java/szewek/flux/container/OnlineMarketContainer.java
@@ -1,10 +1,12 @@
 package szewek.flux.container;
 
 import net.minecraft.entity.merchant.IMerchant;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.MerchantContainer;
 import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import szewek.flux.F;
 import szewek.flux.util.market.ClientOnlineMarket;
@@ -25,5 +27,45 @@ public class OnlineMarketContainer extends MerchantContainer {
 	@Override
 	public ContainerType<?> getType() {
 		return F.C.ONLINE_MARKET;
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
+		ItemStack itemstack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+			if (index == 2) {
+				if (!this.mergeItemStack(itemstack1, 3, 39, true)) {
+					return ItemStack.EMPTY;
+				}
+				slot.onSlotChange(itemstack1, itemstack);
+			} else if (index != 0 && index != 1) {
+				if (index < 30) {
+					if (!this.mergeItemStack(itemstack1, 30, 39, false)) {
+						return ItemStack.EMPTY;
+					}
+				} else if (index < 39 && !this.mergeItemStack(itemstack1, 3, 30, false)) {
+					return ItemStack.EMPTY;
+				}
+			} else if (!this.mergeItemStack(itemstack1, 3, 39, false)) {
+				return ItemStack.EMPTY;
+			}
+
+			if (itemstack1.isEmpty()) {
+				slot.putStack(ItemStack.EMPTY);
+			} else {
+				slot.onSlotChanged();
+			}
+
+			if (itemstack1.getCount() == itemstack.getCount()) {
+				return ItemStack.EMPTY;
+			}
+
+			slot.onTake(playerIn, itemstack1);
+		}
+
+		return itemstack;
 	}
 }
