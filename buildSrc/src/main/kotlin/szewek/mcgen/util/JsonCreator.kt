47d5d9eaeaa931ea72227021ed2eea66ac9c7fe9
Szewek <shevkpl@gmail.com>
2021-04-23 23:56:32 +0200

    Remove JsonCreator wrap inline function

--- buildSrc/src/main/kotlin/szewek/mcgen/util/JsonCreator.kt
+++ buildSrc/src/main/kotlin/szewek/mcgen/util/JsonCreator.kt
@@ -4,13 +4,7 @@ import com.google.gson.JsonElement
 import com.google.gson.internal.Streams
 import com.google.gson.stream.JsonWriter
 
-inline class JsonCreator(val jw: JsonWriter) {
-
-    inline fun wrap(begin: () -> Unit, end: () -> Unit, fn: JsonFunc) {
-        begin()
-        fn()
-        end()
-    }
+class JsonCreator(@JvmField val jw: JsonWriter) {
     fun named(name: String, fn: JsonFunc?) {
         if (fn != null) {
             jw.name(name)
@@ -18,8 +12,16 @@ inline class JsonCreator(val jw: JsonWriter) {
         }
     }
 
-    inline fun obj(fn: JsonFunc) = wrap(jw::beginObject, jw::endObject, fn)
-    inline fun arr(fn: JsonFunc) = wrap(jw::beginArray, jw::endArray, fn)
+    inline fun obj(fn: JsonFunc) {
+        jw.beginObject()
+        fn()
+        jw.endObject()
+    }
+    inline fun arr(fn: JsonFunc) {
+        jw.beginArray()
+        fn()
+        jw.endArray()
+    }
 
     inline infix fun String.obj(crossinline fn: JsonFunc) = named(this) { obj(fn) }
     inline infix fun String.arr(crossinline fn: JsonFunc) = named(this) { arr(fn) }
