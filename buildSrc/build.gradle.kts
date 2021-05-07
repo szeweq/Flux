3434ab2d375b1b66b52e9f278412dc1010587b3a
Szewek <shevkpl@gmail.com>
2021-05-07 19:48:49 +0200

    Use Kotlin 1.5.0

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java-gradle-plugin")
+    kotlin("jvm") version "1.5.0"
+}
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.electronwill.night-config:toml:3.6.3")
+}
+
+sourceSets {
+    main.get().java.srcDirs("src/main/kotlin")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-XXLanguage:+InlineClasses"
+    }
+}
+
+gradlePlugin {
+    plugins {
+        create("mcgenPlugin") {
+            id = "szewek.mcgen.mcgen-plugin"
+            implementationClass = "szewek.mcgen.MCGenPlugin"
+        }
+    }
+}
