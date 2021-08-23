d1022d8e969f56cd5e67e118712aebd55f4deca2
Szewek <shevkpl@gmail.com>
2021-08-23 17:07:28 +0200

    Use Gradle 7.1

--- buildSrc/src/main/kotlin/szewek/mcgen/MCGenPlugin.java
+++ buildSrc/src/main/kotlin/szewek/mcgen/MCGenPlugin.java
@@ -3,6 +3,7 @@ package szewek.mcgen;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.TaskContainer;
 
 import java.io.File;
@@ -13,10 +14,8 @@ public class MCGenPlugin implements Plugin<Project> {
     public void apply(Project project) {
         final File buildDir = project.getBuildDir();
         final TaskContainer tasks = project.getTasks();
-        project.getConvention()
-                .getPlugin(JavaPluginConvention.class)
-                .getSourceSets()
-                .configureEach(sourceSet -> {
+        final JavaPluginExtension javaExt = (JavaPluginExtension) project.getExtensions().getByType(JavaPluginExtension.class);
+        javaExt.getSourceSets().configureEach(sourceSet -> {
                     if (!"test".equals(sourceSet.getName())) {
                         MCGenTasksKt.configureSourceSet(sourceSet, buildDir, tasks);
                     }
